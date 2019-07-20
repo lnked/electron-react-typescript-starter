5c2039b78c343e3aa51f19c7b213b2c3dd2f2969
Edik Bulikyan <ed.proff@gmail.com>
2019-07-20 15:50:59 +0300

    update

--- src/app/main/createWindow.js
+++ src/app/main/createWindow.js
@@ -1,11 +1,13 @@
 import isDev from 'electron-is-dev';
 import installExtension, { REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer'
-import { BrowserWindow } from 'electron';
+import { BrowserWindow, globalShortcut } from 'electron';
 
 const rootPath = process.cwd();
-const publicPath = `${rootPath}/dist`
+const distPath = `${rootPath}/dist`
+const publicPath = `${rootPath}/public`
 
 // https://github.com/electron/electron/blob/master/docs/api/browser-window.md#winsetpositionx-y-animate
+// https://xwartz.gitbooks.io/electron-gitbook/content/en/api/browser-window.html
 
 const createWindow = mainWindow => async () => {
   if(isDev) {
@@ -21,18 +23,24 @@ const createWindow = mainWindow => async () => {
     height: 480,
     resizable: true,
     alwaysOnTop: true,
+    backgroundColor: '#eee',
+    icon: `${distPath}/icons/png/64x64.png`,
     // fullscreen: true,
-    // transparent: true,
+    transparent: true,
     // titleBarStyle: 'hidden',
     // titleBarStyle: 'hiddenInset',
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
-      preload: `${publicPath}/preload.js`,
+      preload: `${distPath}/preload.js`,
     },
   });
 
-  mainWindow.setTitle(require(`${rootPath}/public/config.json`).name);
+  const reloadWindow = () => {
+    mainWindow.reload();
+  }
+
+  mainWindow.setTitle(require(`${publicPath}/config.json`).name);
 
   // mainWindow.isAlwaysOnTop()
 
@@ -44,6 +52,9 @@ const createWindow = mainWindow => async () => {
 
   // mainWindow.setIgnoreMouseEvents(true)
 
+  // mainWindow.setFullScreen(true);
+  // mainWindow.maximize();
+
   mainWindow.setAutoHideMenuBar(true)
 
   mainWindow.setMenuBarVisibility(false)
@@ -53,9 +64,13 @@ const createWindow = mainWindow => async () => {
     // mainWindow.webContents.openDevTools();
   }
   else {
-    // mainWindow.loadURL(`file://${publicPath}/index.html`);
-    mainWindow.loadFile(`${publicPath}/index.html`);
+    // mainWindow.loadURL(`file://${distPath}/index.html`);
+    mainWindow.loadFile(`${distPath}/index.html`);
   }
+
+  mainWindow.on('show', (e) => {
+    setTimeout(reloadWindow, 1500)
+  });
 
   // @TODO: Use 'ready-to-show' event
   // https://github.com/electron/electron/blob/master/docs/api/browser-window.md#using-ready-to-show-event
@@ -78,7 +93,13 @@ const createWindow = mainWindow => async () => {
     e.preventDefault()
   });
 
+  globalShortcut.register('F5', reloadWindow);
+  globalShortcut.register('CommandOrControl+R', reloadWindow);
+
   mainWindow.on('closed', () => {
+    globalShortcut.unregister('F5', reloadWindow);
+    globalShortcut.unregister('CommandOrControl+R', reloadWindow);
+
     mainWindow.removeAllListeners();
     mainWindow = null;
   });
